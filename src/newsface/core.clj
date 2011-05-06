6c806f291265fda8bff569cd094c31920233b527
unknown <Mimi@.(none)>
2011-05-06 19:01:46 +0200

    Started the webapp.

--- src/newsface/core.clj
+++ src/newsface/core.clj
@@ -1,5 +1,26 @@
 (ns newsface.core
-  (:use
-   [newsface persistence ranking]))
+  (:use compojure.core
+	newsface.templates.index
+	[hiccup.middleware :only (wrap-base-url)]
+	[ring.adapter.jetty] :reload)
+  (:require [compojure.route :as route]
+            [compojure.handler :as handler]
+            [compojure.response :as response]))
+
+
+(defroutes *routes*
+  (GET "/" [] (index-page))
+  (route/resources "/")
+  (route/not-found "Page not found"))
+
+
+(def newsface
+  (-> (handler/site *routes*)
+      (wrap-base-url)))
+
+
+(defonce server (run-jetty #'newsface {:port 8080
+				       :join? false}))
+
 
 
---
+++ src/newsface/templates/index.clj
@@ -0,0 +1,38 @@
+(ns newsface.templates.index
+  (:use [hiccup core page-helpers]))
+
+
+(defn index-page []
+  (html5
+    [:head
+      [:title "Newsface - Suggesting you news based on your Facebook friends"]
+      (include-css "/css/style.css")]
+    [:body
+     [:div.header
+      [:a {:href "/"}
+       [:img {:src "/imgs/logo.png"}]]]
+     [:div.menu
+      [:a {:href "/"} "Home"]
+      [:a {:href "/examples.html"} "Examples"]
+      [:a {:href "/"} "Documentation"]
+      [:a {:href "/"} "Contribute"]
+      [:a {:href "/"} "Donate"]]
+     [:div.content
+      [:h2 "What is Newsface?"
+       [:p (str "Newsface [News from Facebook] is a news raccomender tool."
+		"It suggests you news which you may be interested in.")]]
+      [:h2 "Why Newsface is different?"
+       [:p (str "Newsface performs a non-invasive search inside your Facebook "
+		"profile and finds your closest friends (several metrics are"
+		"used). Then extracts a bunch of keywords based on your "
+		"friends tastes and preferences. Finally searches the web, "
+		"bringing you news.")]]]
+     [:div.footer
+      [:div.footer-wrap
+       [:div.contact
+	[:strong "Contact"] [:br]
+	"Support: " [:a {:href "https://github.com/CharlesStain/newsface"}
+		     "Project page"]
+	[:br]
+	"Feedback: " [:a {:href "mailto:alfredo.dinapoli@gmail.com"}
+		      "alfredo.dinapoli@gmail.com"]]]]]))
